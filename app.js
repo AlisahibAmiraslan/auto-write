d0f2a2ed69cecfd0eb2f5e83335b5674bcc961c6
Alisahib Amiraslan <=>
2022-04-27 02:59:28 +0300

    add comments

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+//our text
 let text =
   " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fuga eligendi assumenda delectus ipsam ipsum perspiciatis cupiditate sint esse temporibus, laboriosam nulla accusantium doloribus?";
 
+// our div. which we want to add text to this root div
 const root = document.getElementById("root");
 
+//count from zero to our text length
 let index = 0;
 
+// function which to write on browser
 function textWrite() {
   root.innerText = text.slice(0, index);
   index++;
@@ -13,4 +17,5 @@ function textWrite() {
   }
 }
 
+// time to each second or minute. which you want you can add time
 setInterval(textWrite, 100);
